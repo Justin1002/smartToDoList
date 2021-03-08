7a50ef0fcb36e08e6e823bd9c192d2fc2504a846
Mitchell Gonzalez <mitchell.gonzalez@gmail.com>
2021-03-08 19:22:27 +0000

    initial commit

---
+++ public/scripts/renderTasks.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+});
+
+re
+
+
+
+const renderTasks = (tasks, category) => {
+  $("#tasks-container").empty();
+  if (category === 'all') {
+    //display all categories
+  } else if (category === 'eat') {
+
+  } else if (category === '')
+
+  // for (let tweet of tweets) {
+  //   let newTweet = createTweetElement(tweet);
+  //   $("#tweet-list").prepend(newTweet);
+  // }
+};
