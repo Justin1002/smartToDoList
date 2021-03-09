a5f77dc0df255c144e0cbeb9384fb6160aa210e2
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-03-09 06:37:21 +0000

    Start implementing login/logout buttons in header

---
+++ public/scripts/header.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+  createHeaderDiv('');
+  $('#login-button').click(function(event) {
+    console.log('login button clicked');
+    $.ajax({ method: "GET", url: "/login/1"})
+      .then(function() {
+        createHeaderDiv('Alice Le');
+      });
+  });
+  $('#logout-button').click(function(event) {
+    console.log('logout button clicked');
+    $.ajax({ method: "GET", url: "/logout"})
+      .then(function() {
+        createHeaderDiv('');
+      });
+  });
+});
+
+const createHeaderDiv  = (userName) => {
+  $("#header").empty();
+  const $logo = $(`<i class="fas fa-check-circle"></i>`);
+  $('#header').append($logo);
+  if (userName) {
+    const $div = $(`
+  		<div>
+  			<h1>${userName}</h1>
+  			<button id="logout-button"><i class="fas fa-sign-out-alt"></i></button>
+  		</div>
+		`);
+    $('#header').append($div);
+  } else {
+    const $div = $(`
+  		<div>
+  		<button id="login-button">Login</button>
+  		</div>
+		`);
+    $('#header').append($div);
+  }
+};
